d2cf163d3644daedc4635028a21b37e749142e72
Sithija-R <sithijarathukorale@gmail.com>
2026-08-29 15:34:18 +0530

    first commit

---
+++ auth_service/src/main/java/com/dineflow/auth/model/User.java
@@ -0,0 +1,25 @@
+package com.dineflow.auth.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+public class User {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+}
